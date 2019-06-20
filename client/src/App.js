c17bfbe9a0805ca25f717f7ed45af82cd9cf801b
Kant Destructor <adrien.promtep@epitech.eu>
2019-06-20 15:30:31 +0200

    Add chat form submit

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <div className="Chat">
-            <h1>Cat chat !</h1>
-            <textarea row="1" col="30" placeholder="Enter a message here..."></textarea>
-            <button type="button">Enter</button>
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            messageText: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            messageText: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('Meow !');
+        this.setState({
+            messageText: ''
+        });
+    }
+
+    render() {
+      return (
+        <div className="App">
+            <div className="Chat">
+                <h1>Cat chat !</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                            type="text"
+                            placeholder="Enter a message here..."
+                            onChange={this.handleChange}
+                            value={this.state.messageText}>
+
+                    </input>
+                </form>
+                <button type="button">Enter</button>
+            </div>
         </div>
-    </div>
-  );
+      );
+    }
 }
 
 export default App;
